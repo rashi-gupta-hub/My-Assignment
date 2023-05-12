3b595a6c139fcde3e7bc70b86e51e93d3dd87ae3
rashi-gupta-hub <rashi.gupta_cs19@gla.ac.in>
2023-05-12 16:19:07 +0530

    armstrongQ1

---
+++ src/Assignments/Armstrong.java
@@ -0,0 +1,32 @@
+package Assignments;
+
+import java.util.Scanner;
+
+class ArmstrongOrNot {
+    public boolean armstrongCheck(int n) {
+        int sum = 0;
+        int temp = n;
+        while(temp > 0) {
+            int rem = temp % 10;
+            sum += (rem*rem*rem);
+            temp /= 10;
+        }
+        return (sum == n);
+    }
+}
+
+public class Armstrong {
+    public static void main(String[] args) {
+        ArmstrongOrNot obj = new ArmstrongOrNot();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        boolean isArmstrong = obj.armstrongCheck(n);
+        if(isArmstrong) {
+            System.out.println(n + " is an Armstrong number.");
+        } else {
+            System.out.println(n + " is not an Armstrong number.");
+        }
+        sc.close();
+    }
+}
